e0a486af82d42c2bb27910ad8b0d95cbd69f4827
Banana Viking <bananavikingcoder@gmail.com>
2018-11-01 14:34:24 -0500

    added untracked file

---
+++ ToDoey/ListViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  ToDoey
+//
+//  Created by Banana Viking on 11/1/18.
+//  Copyright © 2018 Banana Viking. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+
+    var itemArray = ["get third app in app store", "find a great job", "move to Colorado"]
+    
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Done", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Add new item here"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+}
+
