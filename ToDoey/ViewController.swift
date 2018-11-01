54718309d42699f369a0330ee5ffcf7710a3fe33
Banana Viking <bananavikingcoder@gmail.com>
2018-11-01 13:13:33 -0500

    added gitignore

---
+++ ToDoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoey
+//
+//  Created by Banana Viking on 11/1/18.
+//  Copyright © 2018 Banana Viking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
